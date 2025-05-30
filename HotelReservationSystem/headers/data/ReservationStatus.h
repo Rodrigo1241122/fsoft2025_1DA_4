36254380223330f02ae5f401560ad05214f22a38
caetanotiago <1241945@isep.ipp.pt>
2025-05-30 12:05:27 +0100

    Adicionados enums RoomType e ReservationStatus + seed com hotéis fictícios

---
+++ HotelReservationSystem/headers/data/ReservationStatus.h
@@ -0,0 +1,11 @@
+//ReservationStatus.h - tiago
+
+#ifndef RESERVATION_STATUS_H
+#define RESERVATION_STATUS_H
+
+enum class ReservationStatus {
+    CONFIRMED,
+    CANCELED
+    };
+
+    #endif
---
+++ HotelReservationSystem/headers/data/RoomType.h
@@ -0,0 +1,12 @@
+//RoomType.h - tiago
+
+#ifndef ROOM_TYPE_H
+#define ROOM_TYPE_H
+
+enum class RoomType {
+    STANDARD,
+    DOUBLE,
+    DELUXE
+};
+
+#endif // ROOM_TYPE_H
