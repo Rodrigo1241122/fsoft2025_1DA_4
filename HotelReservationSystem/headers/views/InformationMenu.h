36d8ccb67c164d4178bb935463ff2a274e55b712
Martim690 <1240690@isep.ipp.pt>
2025-06-03 18:03:34 +0100

    views estrutura

---
+++ HotelReservationSystem/headers/views/InformationMenu.h
@@ -0,0 +1,9 @@
+#ifndef INFORMATIONMENU_H
+#define INFORMATIONMENU_H
+
+class InformationMenu {
+public:
+    void display_information_menu();
+};
+
+#endif /* INFORMATIONMENU_H */
---
+++ HotelReservationSystem/headers/views/PaymentMenu.h
@@ -0,0 +1,9 @@
+#ifndef PAYMENTMENU_H
+#define PAYMENTMENU_H
+
+class PaymentMenu {
+public:
+    void display_payment_menu();
+};
+
+#endif /* PAYMENTMENU_H */
